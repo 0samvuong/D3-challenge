3c1d0333ff524489142ba012d263bed66c108490
0samvuong <101376900+0samvuong@users.noreply.github.com>
2022-08-26 15:10:34 +1000

    a

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+
+d3.csv("assets/data/data.csv").then(function(csvData) {
+
+    // amend data, change int values from str to int 
+    csvData.forEach(data => {
+        data.age = +data.age;
+        console.log(data.abbr);
+        console.log(data.age);
+    })
+
+
+});
